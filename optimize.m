c1a42eab70de27860dca4ebe6e31b4c4e4536e02
shromesh <ryone.y0130@gmail.com>
2024-12-16 23:44:45 +0900

    added some comments into the code

--- optimize.m
+++ optimize.m
@@ -46,10 +46,10 @@ Lpts = round(L/dlx);                        % number of points in the optimizati
 
 Nx = ceil(lambda0*beta/dlx);                % number of grid points in x
 Ny = gap_pts+2*(pos_src + Lpts + spc_pts);  % number of grid points perpendicular to trajectory
- 
+
 nx = floor(Nx/2);
 ny = floor(Ny/2);
-    
+
 % First compute G maximization, then do G/E_max maximization (for comparison)
 for min_G_Emax = (0:1)
     
@@ -60,30 +60,30 @@ for min_G_Emax = (0:1)
     MuR = ones(Nx,Ny);                      % relative permeability grid map
     ER_best = ones(Nx,Ny);                  % storing the best permittivity map
     A_best = 0;
-
+    
     b = zeros(Nx,Ny);                       % TFSF map.  read up on total-field scattered-field if you are interested.
     b(:, pos_src:pos_src + spc_pts + Lpts + gap_pts + Lpts + spc_pts) = 1;  % define the total field region on the grid
     kinc = [0,1];                           % plane wave incident direction (perp. to electron)
-
+    
     RES = [dlx,dly];                        % grid resolution vector
     BC = [-1,-1];                           % boundary condition vector (periodic if -1)
     NPML = [0,0,npml,npml];                 % PML cells on the boundaries (x-,x+,y-,y+)
     Pol= 'Hz';                              % field polarization
     spc = spc_pts*dly;                      % space between source and objects in um
     gap = gap_pts*dly;                      % gap size in um
-
+    
     xs = dlx*(1:Nx);                        % constant to compute the eta object.  x-pos along gap.
-
+    
     delta_device = zeros(Nx,Ny);            % delta_device is 0 where the permittivity doesn't change.  otherwise it is 1 in the optimization region.
     delta_device(1:Nx, pos_src + spc_pts : pos_src + spc_pts + Lpts) = 1;
     delta_device(1:Nx, pos_src + spc_pts + Lpts + gap_pts : pos_src + spc_pts + Lpts + gap_pts + Lpts) = 1;
     delta_device_vec = delta_device(:);     % vector version of delta_device (matlab likes this better)
     
     % define the eta vector field.  see the paper for more details.
-    eta = zeros(Nx,Ny);  
-    eta(:,ny) = 1/Nx*exp(2*pi*1i*dlx*(0:Nx-1)/lambda0/beta);       
+    eta = zeros(Nx,Ny);
+    eta(:,ny) = 1/Nx*exp(2*pi*1i*dlx*(0:Nx-1)/lambda0/beta);
     eta_vec = eta(:);
-
+    
     % define stating permittivity based on what value the 'starting variable'
     % holds
     for i = (1:Nx)
@@ -91,97 +91,97 @@ for min_G_Emax = (0:1)
             if (delta_device(i,j) == 1)
                 if (starting == 1)
                     ER(i,j) = rand*(eps-1)+1;
-                elseif (starting == 2)                
+                elseif (starting == 2)
                     ER(i,j) = eps/2+0.5;
                 else
                 end
             end
         end
     end
-
+    
     % run the simulation with accelerator input (plane wave) but all empty space
     [fields, ~] = FDFD_TFSF(ones(Nx,Ny),MuR,RES,NPML,BC,lambda0,Pol,b,kinc);
-
+    
     % get the fields and the E0 (normalization)
     Ex = fields.Ex;
     Ey = fields.Ey;
     E0 = sqrt(abs(Ex(nx, ny))^2 + abs(Ey(nx, ny))^2);
-
+    
     % define variables to store the iteration progress
     G_best = 0;                 % best gradient
     Gs = zeros(N,1);            % gradients over iteration
     E_maxs = zeros(N,1);        % max E-fields over iteration
     G_by_Es = zeros(N,1);       % G/E over iteration, computed directly
     G_by_Sa = zeros(N,1);       % G/E over iteration, computed with smooth-max
-
+    
     phis = zeros(N,1);          % phase of the maximum accelerating input plane wave
     phi = 0;                    % assume input light phase of 0 to start
     AVM_prev = zeros(Nx,Ny);    % store previous sensitivity information for momentum update
-
+    
     figure(1);                  % open a figure to plot
     
     if ~min_G_Emax
         display('working on gradient maximized structure');
     else
         display('working on acceleration factor maximized structure');
-    end    
+    end
     upd = textprogressbar(N);
     
     for j = (1:N)
-
+        
         upd(j);
         % original simulation (structure in accelerator mode)
         [fields, extra] = FDFD_TFSF(ER,MuR,RES,NPML,BC,lambda0,Pol,b,kinc);
         % get fields
         Ex = fields.Ex/E0;
         Ey = fields.Ey/E0;
-    
-        % compute gradient (see paper)
+        
+        % compute gradient (see paper (23))
         g = sum(sum(eta.*Ex));
         G = real(g);
-
+        
         % get phase
         phis(j) = angle(g);
-
+        
         % get numerical spatial derivative operators
-        DEY = extra.derivatives.DEY;  
-        DEX = extra.derivatives.DEX;    
-    
+        DEY = extra.derivatives.DEY;
+        DEX = extra.derivatives.DEX;
+        
         % turn the permittivity map into a vecor.  Then compute some
         % quantities for later.
-        ER_vec = ER(:);
+        ER_vec = ER(:); % 1列の列ベクトルに変換
         delta_ER = ER > eps/2;
         delta_ER_vec = delta_ER(:);
         chi = delta_device.*(ER - ones(Nx,Ny));
-
+        
         % compute operators from maxwell's eqs. turning Mz into Jx and Jy (Hz into Ex and Ey)
         Ox = -1i*lambda0/2/pi/c0*spdiags(1./ER_vec,0,Nx*Ny,Nx*Ny)*DEY;
         Oy =  1i*lambda0/2/pi/c0*spdiags(1./ER_vec,0,Nx*Ny,Nx*Ny)*DEX;
-
+        
         % create the adjoint vector corresponding to eta (again, see paper)
         eta_aj = [eta_vec; zeros(Nx*Ny,1)];
-
+        
         % if you are evaluating E_max in the material, compute |E| there,
         % otherwise, compute |E| in the full optimization region.
         if (in_material)
             E_abs = (chi/(eps-1)).*sqrt(abs(Ex).^2 + abs(Ey).^2);
         else
             E_abs = delta_device.*sqrt(abs(Ex).^2 + abs(Ey).^2);
-        end        
-
+        end
+        
         % compute auxiliary vectors for later.  (too complicated to explain
         % here.  ask me in person if you're interested).
         x_abs = E_abs(:);
         alpha_vec = exp(x_abs*a);
         alpha_T_1 = sum(alpha_vec);
         Sa = sum(alpha_vec.*x_abs)/alpha_T_1;
-
+        
         X_vec = conj(Ex(:))./x_abs;
         Y_vec = conj(Ey(:))./x_abs;
-
-
+        
+        
         x = [Ex(:); Ey(:)];
-        P = [speye(Nx*Ny) speye(Nx*Ny)]; 
+        P = [speye(Nx*Ny) speye(Nx*Ny)];
         z = conj(x./[x_abs;x_abs]);
         z(isnan(z)) = 0;
         z(isinf(z)) = 0;
@@ -200,62 +200,62 @@ for min_G_Emax = (0:1)
         if (min_G_Emax)
             b_aj = b_aj1 + b_aj2;
         else
-            b_aj = -eta_aj;        
+            b_aj = -eta_aj;
         end
         b_aj = reshape(Ox*b_aj(1:Nx*Ny) + Oy*b_aj(Nx*Ny+1:end),[Nx,Ny]);
         b_aj(isnan(b_aj)) = 0 ;
         
         % get the factored form of the system operator.
         AF = extra.AF;
-
+        
         % run simulation with adjoint source.
         [fields_aj, ~] = FDFD_fast(ER,MuR,RES,NPML,BC,lambda0,Pol,b_aj,AF);
-    
+        
         % get fields
         x_aj = fields_aj.x/E0;
         Ex_aj = reshape(x_aj(1:Nx*Ny),[Nx,Ny]);
         Ey_aj = reshape(x_aj(Nx*Ny+1:end),[Nx,Ny]);
-
+        
         % compute sensitivity information
-        AVM = -real((Ex.*Ex_aj.*delta_device + Ey.*Ey_aj.*delta_device));    
-
+        AVM = -real((Ex.*Ex_aj.*delta_device + Ey.*Ey_aj.*delta_device));
+        
         % record relevant variables in the arrays
         E_max = max(max((E_abs)));
         E_maxs(j) = E_max;
         Gs(j) = G;
         G_by_Es(j) = G/E_max;
         G_by_Sa(j) = G/Sa;
-
+        
         % update permittivity
         ER = ER + alpha*AVM + alpha*gamma*AVM_prev;
-
+        
         % update the previous sensitivity map
         AVM_prev = AVM;
-
+        
         % if permittivity out of bounds, reset inside the correct bounds.
         ER(ER < 1) = 1;
-        ER(ER > eps) = eps;        
-
+        ER(ER > eps) = eps;
+        
         % record best permittivity if applicable
         if (G > G_best)
             ER_best = ER;
         end
-
+        
         % plot stuff without too much hastle, display % done
         if (display_plots && mod(j,skip) == 0)
             %    perc_done = j/N*100
-            clf;         
+            clf;
             subplot(2,2,1);
             disp = [];
             for k = (1:5)
                 disp = [disp; real(ER)];
             end
-
+            
             imagesc(disp,[1,eps])
-            colormap(flipud(gray))    
-            title('relative permittivity')                        
+            colormap(flipud(gray))
+            title('relative permittivity')
             set(findall(gcf,'type','text'),'FontSize',22,'fontWeight','normal')
-            set(gca,'FontSize',22,'fontWeight','normal')    
+            set(gca,'FontSize',22,'fontWeight','normal')
             colorbar()
             
             subplot(2,2,2);
@@ -263,10 +263,10 @@ for min_G_Emax = (0:1)
             plot(Gs(1:j),'k');
             xlabel('iteration number')
             ylabel('gradient (E_0)')
-            title('acceleration gradient at \phi = 0')            
+            title('acceleration gradient at \phi = 0')
             set(findall(gcf,'type','text'),'FontSize',22,'fontWeight','normal')
             set(gca,'FontSize',22,'fontWeight','normal')
-
+            
             subplot(2,2,3);
             plot((1:j),G_by_Es(1:j));
             hold all;
@@ -274,7 +274,7 @@ for min_G_Emax = (0:1)
             xlabel('iteration number')
             ylabel('G/|E|max')
             title('acceleration factor')
-            legend({'actual','smooth-max'})           
+            legend({'actual','smooth-max'})
             set(findall(gcf,'type','text'),'FontSize',22,'fontWeight','normal')
             set(gca,'FontSize',22,'fontWeight','normal')
             
@@ -284,16 +284,16 @@ for min_G_Emax = (0:1)
             plot((1:j),zeros(j,1));
             xlabel('iteration number');
             ylabel('\phi');
-            legend({'computed','\phi=0 (target)'})       
-            title('acceleration phase (\phi)')                        
+            legend({'computed','\phi=0 (target)'})
+            title('acceleration phase (\phi)')
             set(findall(gcf,'type','text'),'FontSize',22,'fontWeight','normal')
-            set(gca,'FontSize',22,'fontWeight','normal')  
+            set(gca,'FontSize',22,'fontWeight','normal')
             
             pause(0.001);
         end
     end
-
-
+    
+    
     %% POST PROCESSING STUFF
     
     % create final field display
@@ -304,16 +304,16 @@ for min_G_Emax = (0:1)
         field_disp = [field_disp;Ex];
         ER_disp = [ER_disp;  (ER-ones(Nx,Ny))*10000];
     end
-
+    
     % plot movie
     NT = 0;         % number of time steps
     %figure(2);
     for t = (1:NT)
-        clf; 
+        clf;
         colormap(redbluecmap)
         imagesc(transpose(ER_disp + real(field_disp*exp(-1i*t/40))),[-5,5]); pause(0.0001);
     end
-
+    
     % force the permittivity distribution binary
     eps_avg = (eps+1)/2;
     ER(ER<eps_avg) = 1;
@@ -323,28 +323,28 @@ for min_G_Emax = (0:1)
     [fields, extra] = FDFD_TFSF(ER,MuR,RES,NPML,BC,lambda0,Pol,b,kinc);
     Ex = fields.Ex/E0;
     Ey = fields.Ey/E0;
-
+    
     % compute the gradient
     g = sum(sum(eta.*Ex));
     G = abs(g);
-
+    
     % get the maximum fields in material and optimization region
     E_abs = delta_device.*sqrt(abs(Ex).^2 + abs(Ey).^2);
     E_max = max(E_abs(:));
-
+    
     E_abs_mat = (ER > 1).*sqrt(abs(Ex).^2 + abs(Ey).^2);
     E_max_mat = max(E_abs_mat(:));
-
+    
     % compute the acceleration factors and save
     if (~min_G_Emax)
-        ER_o = ER;    
+        ER_o = ER;
         G_o = G;
         E_max_o = E_max;
         E_max_mat_o = E_max_mat;
         n_o = G_o/E_max_o;
         n_mat_o = G_o/E_max_mat_o;
-    else    
-        ER_p = ER;    
+    else
+        ER_p = ER;
         G_p = G;
         E_max_p = E_max;
         E_max_mat_p = E_max_mat;
