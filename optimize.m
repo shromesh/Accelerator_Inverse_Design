a86562342ec8e2e07e56c801f627548da82cd221
shromesh <ryone.y0130@gmail.com>
2025-01-14 01:02:55 +0900

    results

--- optimize.m
+++ optimize.m
@@ -11,7 +11,7 @@ display_plots = true;                       % plotting during the run?
 alpha = 5e2;                                % step size in permittivity (~1e2-1e4 works well)
 a = 3;                                     % smooth-max weight factor (see paper)
 beta = 0.5;                                 % ratio of electron speed to speed of light
-N = 1000;                                   % number of iterations
+N = 800;                                   % number of iterations
 
 in_material = false;                        % evaluate E_max in material? or in surrounding regions. (NOTE: it doesn't work well, I would suggest just evaluating in optimization region)
 starting = 0;                               % 0 -> vacuum, 1 -> random, 2 -> midway epsilon
@@ -376,13 +376,13 @@ for min_G_Emax = 0
     % save g1, g2, and G into the same file
     fname_g = sprintf('result/gradients_g1_g2_G_%s.txt', timestamp);
     fileID_g = fopen(fname_g, 'w');
-    fprintf(fileID_g, 'g1: %f\n', g1);
-    fprintf(fileID_g, 'g2: %f\n', g2);
+    fprintf(fileID_g, 'g1: %f + %fi\n', real(g1), imag(g1));
+    fprintf(fileID_g, 'g2: %f + %fi\n', real(g2), imag(g2));
     fprintf(fileID_g, 'G: %f\n', G);
     fprintf(fileID_g, 'G1_best: %f\n', G1_best);
     fprintf(fileID_g, 'G2_best: %f\n', G2_best);
-    fprintf(fileID_g, 'g1_best: %f\n', g1_best);
-    fprintf(fileID_g, 'g2_best: %f\n', g2_best);
+    fprintf(fileID_g, 'g1_best: %f + %fi\n', real(g1_best), imag(g1_best));
+    fprintf(fileID_g, 'g2_best: %f + %fi\n', real(g2_best), imag(g2_best));
     fclose(fileID_g);
     fprintf('File saved as: %s\n', fname_g);
     
