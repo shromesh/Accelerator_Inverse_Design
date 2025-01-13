a1b7a990fefdd9c5e6e47b058969219a0799a222
shromesh <ryone.y0130@gmail.com>
2025-01-13 18:44:17 +0900

    2値化した後の最終の構造をファイルに出力するように．

--- optimize.m
+++ optimize.m
@@ -342,6 +342,15 @@ for min_G_Emax = (0:1)
     g = sum(sum(eta1.*Ex)) + sum(sum(eta2.*Ex));
     G = abs(g);
     
+    % --- 2値化後の最終加速勾配を表示 ---
+    fprintf('\nFinal Acceleration Gradient after Binarization: %f\n', G);
+    
+    % --- ファイルに出力 ---
+    fname = 'final_acceleration_gradient.txt';      % 出力先ファイル名
+    fileID = fopen(fname, 'w');
+    fprintf(fileID, '%f\n', G);
+    fclose(fileID);
+    
     % get the maximum fields in material and optimization region
     E_abs = delta_device.*sqrt(abs(Ex).^2 + abs(Ey).^2);
     E_max = max(E_abs(:));
