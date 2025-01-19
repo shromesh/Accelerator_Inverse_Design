64a863d0f2a0ad6cb03125690ecfb9d13492a7f2
shromesh <ryone.y0130@gmail.com>
2025-01-19 11:12:04 +0900

    綺麗なsingle-channelのグラフができた

--- optimize.m
+++ optimize.m
@@ -11,16 +11,16 @@ display_plots = false;                       % plotting during the run?
 alpha = 5e2;                                % step size in permittivity (~1e2-1e4 works well)
 a = 3;                                     % smooth-max weight factor (see paper)
 beta = 0.5;                                 % ratio of electron speed to speed of light
-N = 700;                                   % number of iterations
+N = 800;                                   % number of iterations
 
 in_material = false;                        % evaluate E_max in material? or in surrounding regions. (NOTE: it doesn't work well, I would suggest just evaluating in optimization region)
 starting = 0;                               % 0 -> vacuum, 1 -> random, 2 -> midway epsilon
 
 grids_in_lam = 100;                         % number of grid points in a free space wavelength
 % grids_in_lam = 100;                         % number of grid points in a free space wavelength
-% gap_nm_values = 200:50:1300;                % gap size in nm variations with step of 10
+gap_nm_values = 200:20:1300;                % gap size in nm variations with step of 10
 % gap_nm_values = 200:100:1300;
-gap_nm_values = [400];
+% gap_nm_values = [400];
 L = 1.0;                                   % size of optimization region (um)
 % NOTE: if this ^ is too big and the epsilon is too large, the simulations
 % can diverge.  This is because there are many degrees of freedom and
@@ -39,7 +39,7 @@ nmax = sqrt(eps);    % refractive index of material region
 gamma = 0.9;                             % 'momentum term', see paper.  Set between 0-1, can speed up simulation in some cases
 
 % output_folder_name = 'result/one_channel_step_10_jan14_2255';
-output_folder_name = 'result/exp_real_G_plot_grids_in_lam_100';
+output_folder_name = 'result/single_channel_step_20_jan18_grids_100';
 
 %% SET OTHER CONSTANTS (DON'T CHANGE)
 dlx = lambda0/grids_in_lam;                 % grid size along electron trajectory axis
